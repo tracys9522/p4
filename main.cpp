875a923292542bdbf68587df7a1bc7b6d9ae9ac7
Max Werner <gaaramax1235@comcast.net>
2018-05-14 18:59:10 -0700

    updated algs

--- main.cpp
+++ main.cpp
@@ -13,6 +13,12 @@ struct compare
   }
 };
 
+struct data
+{
+	int hit;
+	int miss;
+};
+
 priority_queue <process,vector<process>,compare> job_queue; //150
 list <page> page_list;     //100
 
@@ -50,6 +56,88 @@ void generate_job_queue()
   }
 }
 
+void FIFO(struct *data)
+{
+	/*
+	if page is not in table
+		data->miss++;
+			
+		if table is full
+			page_list.pop_front();
+			
+		page_list.push_back(page);
+		
+	*/	
+}
+
+void LRU(struct *data)
+{
+	/*
+	if page is not in table
+		data->miss++;
+			
+		if table is full
+			page_list.pop_front();
+		
+		page_list.push_back(page);
+		
+	if page is in table 
+		page_list.push_back(page);
+	*/	
+}
+
+void LFU(struct *data)
+{
+	/*
+	if page is not in table
+		data->miss++;
+			
+		if table is full
+			remove least referenced
+		
+		page_list.push_back(page);
+		set new page reference to 0
+		set least referenced
+		
+	if page is in table 
+		move page to back and increment reference
+		set least referenced
+	*/
+	
+}
+
+void MFU(struct *data)
+{
+	/*
+	if page is not in table
+		data->miss++;
+			
+		if table is full
+			remove most referenced
+		
+		page_list.push_back(page);
+		set new page reference to 0
+		set most referenced
+		
+	if page is in table 
+		move page to back and increment reference	
+		set most referenced
+	*/
+}
+
+void RAND(struct *data)
+{
+	/*
+	if page is not in table
+		data->miss++;
+			
+		if table is full
+			remove random
+		
+		page_list.push_back(page);
+		
+	*/
+}
 
 int main()
 {
@@ -64,49 +152,46 @@ int main()
 
   cin >> input;
 
-  switch (input) {
-    case 1:
-      FIFO();
-      break;
-    case 2:
-      LRU();
-      break;
-    case 3:
-      LFU();
-      break;
-    case 4:
-      MFU();
-      break;
-    case 5:
-      RAND();
-      break;
-    default:
-      cout << "error" <<endl;
-      return 0;
-  }
-  */
-
-  int page_hit;
-  int page_miss;
-
+  struct data d;
   int seed = time(0);
   srand(seed);
 
-  //for (int i = 0; i < 5; i++)
-  //{
-    page_hit = 0;
-    page_miss = 0;
+  //for (int i = 0; i < 5; i++) {
+		d.hit = 0;
+		d.miss = 0;
 
-    initial_page_list();
-    generate_job_queue();
-
-    //priority queue sorted by arrival time
-    for (int i = 0; i < 150; i++) {
-      cout<<job_queue.top()<<endl;
-      job_queue.pop();
-    }
-
-
+		initial_page_list();
+		generate_job_queue();
+		
+		
+		//simulation
+		
+		switch (input) {
+			case 1:
+			  FIFO(*d);
+			  break;
+			case 2:
+			  LRU(*d);
+			  break;
+			case 3:
+			  LFU(*d);
+			  break;
+			case 4:
+			  MFU(*d);
+			  break;
+			case 5:
+			  RAND(*d);
+			  break;
+			default:
+			  cout << "error" <<endl;
+			  return 0;
+		}
+	
+		//priority queue sorted by arrival time
+		for (int i = 0; i < 150; i++) {
+		  cout<<job_queue.top()<<endl;
+		  job_queue.pop();
+		}
 
   //}
 }
