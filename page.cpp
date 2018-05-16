674dfcc48cab86ef2b78b32ed2b3afa6a033557b
Erik Trewitt <etrewitt@users.noreply.github.com>
2018-05-16 05:33:45 -0700

    rename getters/setters to be less lengthy

--- page.cpp
+++ page.cpp
@@ -3,7 +3,7 @@
 page::page()
 {
   _process_id = -1;
-  _page_id = -1;
+  _addr = -1;
   // _arrival_time = -1;
   _last_referenced = -1;
   _frequency = 0;
@@ -20,10 +20,10 @@ page::page()
 //   _frequency = 0;
 //   _memory = false;
 // }
-page::page(int process_id, int page_id, int page_size)
+page::page(int process_id, int addr, int page_size)
 {
   _process_id = process_id;
-  _page_id = page_id;
+  _addr = addr;
   // _arrival_time = arrival;
 
   _last_referenced = -1;
@@ -40,15 +40,15 @@ page::page(int process_id, int page_id, int last_referenced, int frequency)
   _frequency = frequency;
 }
 */
-int page::get_process_id()const{ return _process_id; }
-int page::get_page_id()const{ return _page_id; }
+int page::get_pid()const{ return _process_id; }
+int page::get_addr()const{ return _addr; }
 // int page::get_arrival_time()const {return _arrival_time; }
 int page::get_last_ref()const{ return _last_referenced; }
 int page::get_frequency()const{ return _frequency; }
 bool page::in_memory()const{ return _memory == 0? false : true; }
 
-void page::set_process_id(int process_id){ _process_id = process_id; }
-void page::set_page_id(int page_id){ _page_id = page_id; }
+void page::set_pid(int process_id){ _process_id = process_id; }
+void page::set_addr(int addr){ _addr = addr; }
 void page::set_last_ref(int last_referenced){ _last_referenced = last_referenced; }
 void page::set_frequency(int frequency){ _frequency = frequency; }
 void page::set_memory(bool memory){ _memory = memory; }
@@ -59,6 +59,6 @@ void page::free_page(int time){
 
 std::ostream &operator <<(std::ostream &ostr, const page pg)
 {
-    ostr << "page pid: " << pg.get_process_id() << ", page id: " << pg.get_page_id();
+    ostr << "page pid: " << pg.get_pid() << ", page id: " << pg.get_addr();
     return ostr;
 }
--- page.h
+++ page.h
@@ -5,7 +5,7 @@
 class page
 {
   int _process_id;
-  int _page_id;
+  int _addr;
   // int _arrival_time;
   int _last_referenced; //timestamp
   int _frequency;
@@ -15,18 +15,18 @@ class page
 public:
   page();
   // page(int page_id, int arrival_time);
-  page(int process_id, int page_id, int page_size);
+  page(int process_id, int addr, int page_size);
   //page(int process_id, int page_id, int _last_referenced, int frequency);
 
-  int get_process_id() const;
-  int get_page_id() const;
+  int get_pid() const;
+  int get_addr() const;
   // int get_arrival_time() const;
   int get_last_ref() const;
   int get_frequency() const;
   bool in_memory() const;
 
-  void set_process_id(int process_id);
-  void set_page_id(int page_id);
+  void set_pid(int process_id);
+  void set_addr(int addr);
   // void set_arrival_time(int arrival);
   void set_last_ref(int last_referenced);
   void set_frequency(int frequency);
